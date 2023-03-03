e71543561e7151cf465b7a28a99697a7eaf737fd
Deyvid Oliveira Silva <Deyvid4900@gmail.com>
2023-03-02 22:21:27 -0300

    ProjetoSiteApi

---
+++ MoviePlace/src/App.jsx
@@ -0,0 +1,21 @@
+
+import {Outlet} from "react-router-dom"
+import './App.css'
+import Navbar from "./components/navbar"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='App'>
+      <Navbar/>
+      
+      <Outlet/>
+    </div>
+    </>
+  )
+}
+
+export default App
